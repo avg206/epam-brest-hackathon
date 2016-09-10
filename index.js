42aedd72cbde514c84f1f8d61709e05d8e0898b7
Anatoli Huseu1 <avg.tolik@gmail.com>
2016-09-10 12:37:52 +0300

    Init app structure

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 require('babel-core/register');
 require('babel-polyfill');
 
-const fs = require('fs');
-const https = require('https');
-const config = require('./config/backend.js');
-const apiServer = require('./backend/server').default;
-require('./backend/socket');
+const server = require('./server/server.js');
 
-const PORT = process.env.PORT || config.apiPort;
-const ENV = process.env.NODE_ENV;
-
-if (ENV === 'production') {
-  apiServer.listen(PORT, () => {
-    console.log(`Server listening on: ${PORT}`);
-  });
-} else {
-  https.createServer({
-    key: fs.readFileSync('./ssl/key.pem'),
-    cert: fs.readFileSync('./ssl/cert.pem'),
-    passphrase: 'qwerty',
-  }, apiServer).listen(PORT, () => {
-    console.log(`Server listening on: ${PORT}`);
-  });
-}
+server();
---
+++ server/server.js
@@ -0,0 +1,7 @@
+
+
+
+
+export default () => {
+  // Here init server
+};
