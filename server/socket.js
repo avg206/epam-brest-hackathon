8b76d6b4aabeed266a635dfd46a47153d5a09e19
Denis Karlyuk <Denis_Karlyuk@epam.com>
2016-09-10 16:19:32 +0300

    Fix socket

--- server/socket.js
+++ server/socket.js
@@ -4,32 +4,26 @@ import config from '../config/assigne.json';
 
 const io = socket();
 
-io.on('connect', (so) => {
-  Task.find()
-    .then((tasks) => {
-      so.emit('tasks', tasks);
-      so.emit('assigne', config);
-    });
+io.on('connect', async(so) => {
+  const tasks = await Task.find();
+  so.emit('tasks', tasks);
+  so.emit('assigne', config);
 
-  so.on('new task', (newTask) => {
-    const task = new Task(newTask);
-    task.save();
-    io.emit('new task', newTask);
+  so.on('new task', async(newTask) => {
+    let task = new Task(newTask);
+    task = await task.save();
+    io.emit('new task', task);
   });
 
-  so.on('delete task', (id) => {
-    Task.findOneAndRemove(id)
-      .then(() => {
-        io.emit('delete task', id);
-      });
+  so.on('delete task', async(id) => {
+    await Task.findOneAndRemove(id);
+    io.emit('delete task', id);
   });
 
-  so.on('update task', (update) => {
-    Task.findOneAndUpdate({ _id: update.id }, update, { new: true })
-      .then((task) => {
-        if (!task) so.emit('error', 'Task deleted');
-        else io.emit('update task', task);
-      });
+  so.on('update task', async(update) => {
+    const task = await Task.findOneAndUpdate({ _id: update.id }, update, { new: true });
+    if (!task) so.emit('error', 'Task deleted');
+    else io.emit('update task', task);
   });
 });
 
