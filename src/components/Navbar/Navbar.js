db029c1ec82e3289abf4907425b8b48896ad8b3e
Anatoli Huseu1 <avg.tolik@gmail.com>
2016-09-10 14:56:16 +0300

    Update Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,9 @@
 import React from 'react';
 
 const Navbar = () => (
-  <div className="ui inverted top fixed menu">
-    <a className="active item">
-      Home
-    </a>
-    <a className="item">
-      Messages
-    </a>
-    <a className="item">
-      Friends
-    </a>
-    <div className="right menu">
-      <div className="item">
-        <div className="ui icon input">
-          <input type="text" placeholder="Search..." />
-          <i className="search link icon" />
-        </div>
-      </div>
-      <a className="ui item">
-        Logout
-      </a>
+  <div className="ui grey inverted top fixed menu">
+    <div className="header item">
+      Office Help Desk
     </div>
   </div>
 );
