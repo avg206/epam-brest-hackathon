5de45258615ef767f63c43c2b339104374140683
Denis Karlyuk <Denis_Karlyuk@epam.com>
2016-09-10 13:12:46 +0300

    Add server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require('babel-core/register');
 require('babel-polyfill');
 
-const server = require('./server/server.js').default;
-server();
+const server = require('./server/server').default;
+const socket = require('./server/socket').default;
+const http = require('http');
+
+const serverApp = http.createServer(server)
+  .listen(3400, () => console.log('Server listen port 3400'));
+
+socket.listen(serverApp);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
-import socket from './socket';
 import express from 'express';
 
 const app = express();
-app.listen(3400, () => console.log('Server listen 3400'));
 
-socket.listen(app);
 export default app;
